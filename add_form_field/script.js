8569ef24698454bd919669c25259337a7a3e4a0a
ibrahimohid <ibrahimohid77@gmail.com>
2024-05-04 21:49:37 +0600

    add & remove form field final updated

--- add_form_field/script.js
+++ add_form_field/script.js
@@ -1,17 +1,20 @@
 $(document).ready(function () {
-    var max = 5;
-    var x = 1;
     $('button').click(function () {
+        var max = 5;
+        var x = 1;
         $('.category-form').append('<div class="input-category"><input type="text"name="category-name" id="category-id" placeholder="EnterCategory Name"><div class="delete-btn"><a href=""><i class="fa-solid fa-trash"></i></a></div></div>');
-    
-        if(x < max){
+
+        if (x < max) {
             x++;
-        }else{
+        } else {
             alert('Only Five Category Added');
         }
         $('.delete-btn').click(function (e) {
             e.preventDefault();
             $(this).parent().remove();
         });
+    });
+    $('#clear-btn').click(function () {
+        $('.input-category').remove();
     });
 });
